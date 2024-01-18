fdf04435d5c969888e06589d91c625adec57f638
antoninbrdl24 <antonin.brondel24@gmail.com>
2024-01-18 08:57:29 +0100

    formulaire de connexion , login , logout , reset password

---
+++ src/Controller/LogoutController.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LogoutController extends AbstractController
+{
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): Response
+    {
+        return new Response('Logout');
+    }
+}
