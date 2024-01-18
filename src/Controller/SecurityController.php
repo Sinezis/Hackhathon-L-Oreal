f17ab2f8e6836f08b4c10aa2f4808f25fc735c34
antoninbrdl24 <antonin.brondel24@gmail.com>
2024-01-19 00:41:59 +0100

    redirection

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -91,6 +91,6 @@ class SecurityController extends AbstractController
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_index');
+        return $this->redirectToRoute('app_login');
     }
 }
